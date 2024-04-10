fe11903d13e672f30bc3884eba09263bd41294c2
Harsh Walawalkar <hwalawalkar@gmail.com>
2024-04-10 22:23:15 +0530

    Version 0.1 - Added comments for all functions - Fixed Auto Rearrangement to enable tabs already in groups to move - Better UI with tabs, buttons and options menu

--- background.js
+++ background.js
@@ -1,8 +1,129 @@
 sites = new Set();
 
-chrome.webNavigation.onCommitted.addListener((newTabDetails) => {
+// Returns the short URL for the specified tab and adds URL to the set of all URLs
+function fetchURL(input) { 
+  let urlObj = new URL(input);
+  sites.add(String(urlObj.hostname));
+  return String(urlObj.hostname);
+};
+
+// Create Tab Group from grouped IDs
+async function createTabGroups(groupedIDS, site) { 
+  console.log("groupedIDS", groupedIDS)
+  await chrome.tabs.group({ tabIds: groupedIDS }, function(group) { // Await for group  to be created
+    console.log(group);
+    moveGroups(group);
+    console.log("Created new group", group); //remove
+    //updateGroupInfo(group, site);
+  });
+  return true;
+};
+
+// Update Tab Group details :: TODO: Modify to generic class to update tab name
+function updateGroupInfo(groupID, site) { 
+  let groupName = site; 
+  chrome.tabGroups.update(groupID, { title: groupName}, function(updatedGroup) {
+    console.log("Updated group", updatedGroup);
+  });
+};
+
+// Remove tabs from groups
+function destroyGroups(input) { 
+  if (typeof input == "object") { // Pass input as tab objects and ungroup all tabs in group
+    input.forEach(tab => {
+      chrome.tabs.ungroup(tab.id , function() {
+        console.log("Ungrouped tabs");
+      });
+    });
+  }
+  
+  if (typeof input == "number") { // Pass input as tab number and ungroup individually from group
+    chrome.tabs.ungroup(input , function() { 
+      console.log("Ungrouped tabs");
+    })
+  }
+};
+
+// Move groups to 0 index
+function moveGroups(groupID) {  
+    chrome.tabGroups.move(groupID, {index: 0}, function(tabGroup) {
+      console.log("Moved group", tabGroup);
+    });
+};
+
+// Grouping tabs with the same shortURL into a array of objects
+function groupTabs(tabInput) { 
+  for (let site of sites) { // Loop over list of unique sites
+    let groupedIDS = [];
+    for (let tab of tabInput) { // Loop over list of open tabs
+      let id = tab.id;
+      let shorturl = tab.shorturl; 
+      if (shorturl == site) { // If url of new tab already exists in another tab add it to list
+        groupedIDS.push(id);
+      }
+    }
+    if (groupedIDS.length > 1) { // If tab is not unique add it to group or create new group
+      groupID = createTabGroups(groupedIDS, site);
+    }
+  };
+};
+
+// Adding tab to target group
+async function addNewTabtoGroup(targetGroupID, newTabID) {
+  await chrome.tabs.group({ groupId: targetGroupID, tabIds: newTabID });
+};
+
+// Assign tab position based on condition
+function assignNewTab(newTabDetails) {
+  let allGroupTabs = {};
+  let newTabID = newTabDetails.tabId;
+  let newTabURL = fetchURL(newTabDetails.url);
+  chrome.tabs.query({ currentWindow: true}, function (tabs) { // Compare new tab URL with each tab URL and group into dict
+    tabs.forEach(tabs => {
+      tabs.shorturl = fetchURL(tabs.url);
+      if (newTabURL == tabs.shorturl) {
+        let tab = tabs.id;
+        allGroupTabs[tab] =  tabs.groupId;
+      }
+    });
+    let groupIDs = Object.values(allGroupTabs);
+    let tabIDs = Object.keys(allGroupTabs);
+
+    if (groupIDs.some(value => value !== -1)) { // Reposition tab to old group or different group
+      let targetGroupID = groupIDs.filter(value => value !== -1);
+      addNewTabtoGroup(targetGroupID[0], newTabID);
+    }
+
+    else if (groupIDs.length > 1) { // Group with other single tabs
+      tabIDs = tabIDs.map(Number);
+      createTabGroups(tabIDs, newTabURL);
+    }
+  })
+};
+
+// Retrieve specified storage value based on key
+const getStorageOption = async (key) => { 
+  return new Promise((resolve, reject) => {
+    chrome.storage.sync.get([key], function(result) {
+      if (result[key] === undefined) {
+        reject();
+      } else {
+        return resolve(result[key]);
+      }
+    });
+  });
+};
+
+// Listener for tab change events
+chrome.webNavigation.onCommitted.addListener((newTabDetails) => { 
   if (newTabDetails.frameId === 0) {
-    assignNewTab(newTabDetails);
+    getStorageOption("autoRearr").then((data) => { // Check if Auto Rearrange is enabled
+      console.log('Data: ' + data);
+      if (data == true) { 
+        destroyGroups(newTabDetails.tabId);
+        assignNewTab(newTabDetails);
+      }
+    })
   }
 });
 
@@ -13,8 +134,8 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => { // Lis
   if (request.task === "url") { // Sort based on the URL
     tabsArray.forEach(tabsArray => {
     tabsArray.shorturl = fetchURL(tabsArray.url);
-  });
-  results = groupTabs(tabsArray)
+    });
+    results = groupTabs(tabsArray)
   }
 
   else if (request.task === "context") { // Sort based on the Context
@@ -35,96 +156,6 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => { // Lis
   else if (request.task === "sort") {
 
   }
-
-  return true;
 });
 
-function fetchURL(input) { // Returns the short URL for the specified tab and adds URL to the set of all URLs
-  let urlObj = new URL(input);
-  sites.add(String(urlObj.hostname));
-  return String(urlObj.hostname);
-};
-async function createTabGroups(groupedIDS, site) { // Create Tab Group from grouped IDs
-  console.log("groupedIDS", groupedIDS)
-  await chrome.tabs.group({ tabIds: groupedIDS }, function(group) {
-    console.log(group);
-    moveGroups(group);
-    console.log("Created new group", group); //remove
-    //updateGroupInfo(group, site);
-  });
-};
 
-function updateGroupInfo(groupID, site) { // Update Tab Group name
-  let groupName = site; 
-  chrome.tabGroups.update(groupID, { title: groupName}, function(updatedGroup) {
-    console.log("Updated group", updatedGroup);
-  });
-};
-
-function destroyGroups(input) { // Remove all tab groups
-  input.forEach(tab => {
-    chrome.tabs.ungroup(tab.id , function() {
-      console.log("Ungrouped tabs");
-    });
-  });
-};
-
-async function moveGroups(groupID) {  // Move groups to 0 index
-  try {
-    await chrome.tabGroups.move(groupID, {index: 0}, function(tabGroup) {
-      console.log("Moved group", tabGroup);
-    });
-  } catch (error) {
-    console.log("Error while making group", error);
-  }
-};
-
-function groupTabs(tabInput) { // Grouping tabs with the same shortURL into a array of objects
-  for (let site of sites) {
-    let groupedIDS = [];
-    for (let tab of tabInput) {
-      let id = tab.id;
-      let shorturl = tab.shorturl;
-      if (shorturl == site) {
-        groupedIDS.push(id);
-      }
-    }
-    if (groupedIDS.length > 1) {
-      groupID = createTabGroups(groupedIDS, site);
-    }
-  };
-};
-
-async function addNewTabtoGroup(targetGroupID, newTabID) {
-  await chrome.tabs.group({ groupId: targetGroupID, tabIds: newTabID });
-};
-
-function assignNewTab(newTabDetails) {
-  let allGroupTabs = {};
-  let newTabID = newTabDetails.tabId;
-  let newTabURL = fetchURL(newTabDetails.url);
-  chrome.tabs.query({ currentWindow: true}, function (tabs) {
-    tabs.forEach(tabs => {
-      tabs.shorturl = fetchURL(tabs.url);
-      if (newTabURL == tabs.shorturl) {
-        let tab = tabs.id;
-        allGroupTabs[tab] =  tabs.groupId;
-      }
-    });
-    console.log(allGroupTabs);
-    let groupIDs = Object.values(allGroupTabs);
-    let tabIDs = Object.keys(allGroupTabs);
-
-    if (groupIDs.some(value => value !== -1)) {
-      let targetGroupID = groupIDs.filter(value => value !== -1);
-      console.log(targetGroupID+ " " + newTabID + " " + newTabURL);
-      addNewTabtoGroup(targetGroupID[0], newTabID);
-    }
-
-    else if (groupIDs.length > 1) {
-      tabIDs = tabIDs.map(Number);
-      console.log("tabIDs ", tabIDs)
-      createTabGroups(tabIDs, newTabURL);
-    }
-  })
-};
--- popup.js
+++ popup.js
@@ -2,8 +2,38 @@
     let tabsList, listItem;
 })();
 
-window.addEventListener("DOMContentLoaded", (event) => { // Add event listener after DOM content loaded
+// Add event listener after DOM content loaded
+window.addEventListener("DOMContentLoaded", (event) => { 
+    const buttons = document.querySelectorAll('.button-switcher');
+    const checkboxes = document.querySelectorAll("input[type='checkbox']");
+    const linkedBoxes = {"autoRearr": "aggrRearr"}
     
+    async function restoreOptions (checkboxes) { // Restore settings for extension
+        for (let checkbox of checkboxes) { 
+            await new Promise((resolve, reject) => { // Promise to ensure that data is loaded before funciton proceeds
+                chrome.storage.sync.get(checkbox.id, (result) => {
+                    if (chrome.runtime.lastError) {
+                        reject(chrome.runtime.lastError);
+                    } else { // Resolve if data is loaded and update checkbox state
+                        checkbox.checked = result[checkbox.id] || false;
+                        resolve();
+                    }
+                });
+            });
+        }
+    };
+    
+    restoreOptions(checkboxes); // Calling restoreOptions
+
+    // Add listener for each navbar button
+    buttons.forEach((button) => { 
+        button.addEventListener('click', (event) => {
+            const sectionNumber = event.target.getAttribute('data-section');
+            showSection(sectionNumber);
+        });
+    });
+
+    // Listener to accept URL Sort button
     document.getElementById('urlSort').addEventListener('click', async () => { // Async function to sort items based on URL
         chrome.tabs.query({ currentWindow: true }, function (tabs) {
             chrome.runtime.sendMessage({tabs: tabs, task: "url"}, function(response) { // Call to background.js
@@ -11,18 +41,50 @@ window.addEventListener("DOMContentLoaded", (event) => { // Add event listener a
         })
     });
 
+    // Listener to accept Context Sort button
     document.getElementById('contextSort').addEventListener('click', async () => { // Async function to sort items based on Context
         chrome.tabs.query({ currentWindow: true }, function (tabs) {
             chrome.runtime.sendMessage({tabs: tabs, task: "context"}, function(response) { // Call to background.js
             })
         })
     });
-
+    
+    // Listener to accept Reset Groups button
     document.getElementById('destroyGroups').addEventListener('click', async () => { // Destroy groups
         chrome.tabs.query({ currentWindow: true }, function (tabs) {
             chrome.runtime.sendMessage({tabs: tabs, task: "destroy"}) // Call to background.js
         })
     });
+
+    // Function to show the selected section
+    function showSection(sectionNumber) {
+        const sections = document.querySelectorAll('.section');
+        sections.forEach((section) => { // Remove active to collapse each section
+            section.classList.remove('active');
+        });
+        document.getElementById(`section${sectionNumber}`).classList.add('active'); // Activate required section
+    }
+
+    // Event listener for each checkbox in options menu
+    checkboxes.forEach((checkbox) => {
+        checkbox.addEventListener('change', (event) => {
+            checklinkedBox();
+            saveOptions(event);
+        });
+    });
+
+    // Function to check checkboxes that are depended on each other
+    function checklinkedBox(checkbox) {
+        
+    };
+
+    // Save current option settings into Chrome Storage
+    function saveOptions (event) {
+        let obj = {};
+        obj[event.target.id] = event.target.checked;
+        chrome.storage.sync.set(obj, () => {
+        });
+    }
 });
 
 
